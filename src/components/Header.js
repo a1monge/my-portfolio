edf4b2fbdc904e505dbdab9cf2150255443ce4c2
Alex Monge <monge.alex572@gmail.com>
2024-07-06 11:57:51 -0500

    finished rough about me page

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,12 @@ function Header() {
         <div className="container mt-4">
             <header className="header text-center">
                 <div className="row align-items-center">
-                    <div className="col-md-8">
-                        <h1 className="mt-4">Alex Monge</h1>
-                        <p className="lead">Hello! I am an upcoming graduate passionate about cloud computing and eager to pursue a career as an application developer.</p>
-                        <a href="#get-started" className="btn btn-primary btn-lg">Let's get started</a>
+                    <div className="col-md-8 text-left" style={{ textAlign: "left", marginLeft: "0" }}>
+                        <h1 className="mt-4 text-left">
+                            Alex Monge</h1> 
+                        <p className="lead">
+                            Hello! I am an upcoming graduate passionate about cloud computing and eager <br/>to pursue a career as an application developer.</p>
+                        <a href="#Featured-Projects" className="btn btn-primary btn-lg">Featured Projects</a>
                     </div>
                     <div className="col-md-4">
                         <img src={profileImage} alt="Alex Monge" className="rounded-circle img-fluid" style={{ maxWidth: "100%", height: "auto" }} />
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,25 +1,29 @@
 // Skills.js
 import React from 'react';
-
+import python from '../img/python-logo.png'; // Import the image
+import java from '../img/java-logo.png'; // Import the image
+import node from '../img/node-logo.png'; // Import the image
+import sql from '../img/sql-logo.png'; // Import the image
+import react from '../img/react-logo.png'; // Import the image
 const Skills = () => {
     return (
         <div className="container text-center mt-5">
             <h3>Top Skills</h3>
             <div className="d-flex justify-content-around flex-wrap">
                 <div className="skill m-3" style={{ width: "150px" }}>
-                    <h5>Python</h5>
+                    <img src={python} alt="python logo" style={{ maxWidth: "100%", height: "auto" }} />
                 </div>
                 <div className="skill m-3" style={{ width: "150px" }}>
-                    <h5>React.js</h5>
+                    <img src={java} alt="python logo" style={{ maxWidth: "100%", height: "auto" }} />
                 </div>
                 <div className="skill m-3" style={{ width: "150px" }}>
-                    <h5>Java</h5>
+                    <img src={react} alt="react logo" style={{ maxWidth: "100%", height: "auto" }} />
                 </div>
                 <div className="skill m-3" style={{ width: "150px" }}>
-                    <h5>SQL</h5>
+                    <img src={sql} alt="sql logo" style={{ maxWidth: "100%", height: "auto" }} />
                 </div>
                 <div className="skill m-3" style={{ width: "150px" }}>
-                    <h5>Node.js</h5>
+                    <img src={node} alt="node,js logo" style={{ maxWidth: "100%", height: "auto" }} />
                 </div>
             </div>
         </div>
