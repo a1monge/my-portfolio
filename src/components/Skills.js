12c1287196bc909dea83df788be5a0475f46f899
Alex Monge <monge.alex572@gmail.com>
2024-07-07 20:45:19 -0500

    done for now

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,28 +2,29 @@
 import React from 'react';
 import python from '../img/python-logo.png'; // Import the image
 import java from '../img/java-logo.png'; // Import the image
-import node from '../img/node-logo.png'; // Import the image
+import Django from '../img/Django.png'; // Import the image
 import sql from '../img/sql-logo.png'; // Import the image
 import react from '../img/react-logo.png'; // Import the image
+
 const Skills = () => {
     return (
         <div className="container text-center mt-5">
             <h3>Top Skills</h3>
-            <div className="d-flex justify-content-around flex-wrap">
-                <div className="skill m-3" style={{ width: "150px" }}>
-                    <img src={python} alt="python logo" style={{ maxWidth: "100%", height: "auto" }} />
+            <div className="d-flex justify-content-center flex-wrap">
+                <div className="skill m-3">
+                    <img src={python} alt="Python Logo" style={{ maxWidth: "100%", height: "auto" }} />
                 </div>
-                <div className="skill m-3" style={{ width: "150px" }}>
-                    <img src={java} alt="python logo" style={{ maxWidth: "100%", height: "auto" }} />
+                <div className="skill m-3">
+                    <img src={java} alt="Java Logo" style={{ maxWidth: "100%", height: "auto" }} />
                 </div>
-                <div className="skill m-3" style={{ width: "150px" }}>
-                    <img src={react} alt="react logo" style={{ maxWidth: "100%", height: "auto" }} />
+                <div className="skill m-3">
+                    <img src={react} alt="React Logo" style={{ maxWidth: "100%", height: "auto" }} />
                 </div>
-                <div className="skill m-3" style={{ width: "150px" }}>
-                    <img src={sql} alt="sql logo" style={{ maxWidth: "100%", height: "auto" }} />
+                <div className="skill m-3">
+                    <img src={sql} alt="SQL Logo" style={{ maxWidth: "100%", height: "auto" }} />
                 </div>
-                <div className="skill m-3" style={{ width: "150px" }}>
-                    <img src={node} alt="node,js logo" style={{ maxWidth: "100%", height: "auto" }} />
+                <div className="skill m-3">
+                    <img src={Django} alt="Django Logo" style={{ maxWidth: "100%", height: "auto" }} />
                 </div>
             </div>
         </div>
@@ -31,4 +32,3 @@ const Skills = () => {
 }
 
 export default Skills;
-
