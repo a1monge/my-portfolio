72a63493b527f8cd78901dcd154aaa50875cf660
Alex Monge <monge.alex572@gmail.com>
2024-07-15 22:05:01 -0500

    1

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -4,35 +4,37 @@ import projectImage from '../img/project-placeholder.jpg';
 import collab from '../img/collab.png';
 import galaga from '../img/Galaga.png';
 import restaurant from '../img/restaurant.png';
+import dealership from '../img/deployed_loggedin.png'
 
 const Featured = () => {
     const projects = [
         {
-            title: "WildlifeGrid",
+            title: "Wildlife Grid",
             description: "WildlifeGrid is a full stack web application developed using HTML, CSS, JavaScript, PHP, and SQL. I was part of a collaborative effort to monitor and manage the distribution of flora and fauna. The project aimed to raise awareness about local ecosystems and facilitate community involvement in wildlife monitoring.",
-            link: "https://github.com/a1monge/WildlifeGrid.com.git",
+            appLink: "https://a1monge.github.io/WildlifeGrid.com/index.html",
+            githubLink: "https://github.com/a1monge/WildlifeGrid.com.git",
             img: collab
         },
-
         {
-            title: "Restaurant Website",
+            title: "Car Dealership",
+            description: "The Full Stack Developer Capstone Project is a comprehensive demonstration of skills in developing a web application from scratch, incorporating both front-end and back-end components.",
+            appLink: null, // Example with no appLink
+            githubLink: "https://github.com/a1monge/xrwvm-fullstack_developer_capstone.git",
+            img: dealership
+        },
+        {
+            title: "Restaurants",
             description: "This website is a fully responsive web application developed using React. The project showcases my front-end development skills, including dynamic content rendering, responsive design with Bootstrap, and modern, intuitive interfaces.",
-            link: "https://github.com/a1monge/Restaurant-Website.git",
+            appLink: null,
+            githubLink: "https://github.com/a1monge/Restaurant-Website.git",
             img: restaurant
         },
-
         {
             title: "Galaga Game",
-            description: "This was a personal project in which I created an imitation game of the old arcade game Galaga using JavaScript and HtML. In this game you control a spaceship at the bottom of the screen. Your objective is to shoot down the waves of enemies that appear from the top of the screen while avoiding their projectiles. The game keeps track of your score, which increases as you destroy enemies. The game ends when an enemy projectile hits your spaceship.",
-            link: "https://github.com/a1monge/Galaga.git",
+            description: "This was a personal project in which I created an imitation game of the old arcade game Galaga using JavaScript and HTML. In this game you control a spaceship at the bottom of the screen. Your objective is to shoot down the waves of enemies that appear from the top of the screen while avoiding their projectiles. The game keeps track of your score, which increases as you destroy enemies. The game ends when an enemy projectile hits your spaceship.",
+            appLink: "https://a1monge.github.io/Galaga_Game/",
+            githubLink: "https://github.com/a1monge/Galaga_Game.git",
             img: galaga
-        },
-
-        {
-            title: "Coming Soon!",
-            description: "Full stack Independent Project",
-            link: "https://github.com/username/project-three",
-            img: projectImage
         },
 
     ];
@@ -49,7 +51,10 @@ const Featured = () => {
                                     <div className="card-body">
                                         <h5 className="card-title">{project.title}</h5>
                                         <p className="card-text">{project.description}</p>
-                                        <a href={project.link} className="btn btn-primary" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+                                        {project.appLink && ( // Conditionally render if appLink exists
+                                            <a href={project.appLink} className="btn btn-primary" target="_blank" rel="noopener noreferrer">View Static Application</a>
+                                        )}
+                                        <a href={project.githubLink} className="btn btn-primary" target="_blank" rel="noopener noreferrer">View on GitHub</a>
                                     </div>
                                 </div>
                                 <div className="col-md-4">
